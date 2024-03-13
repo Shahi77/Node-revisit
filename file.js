959036cb779b7391411ee43e7eb6a0cdc7bef755
Shahi77 <shahiinfinity02@gmail.com>
2024-03-13 20:57:06 +0530

    file handling

---
+++ file.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+fs.writeFileSync("./test.txt", "Hello There!!");
+
+fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+
+const result = fs.readFileSync("./test.txt", "utf-8");
+console.log(result);
+
+//fs.unlinkSync("./test.txt"); //To delete file
+console.log(fs.statSync("./test.txt")); // status of file
+
+/*
+Async
+fs.writeFile("./test.txt", "Hey There!!", (err) => {});
+fs.readFile("./test.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log(result);
+  }
+}); */
