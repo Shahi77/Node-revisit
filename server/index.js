db87153b47f1b5beffcdddf09efbcad06f87e615
Shahi77 <shahiinfinity02@gmail.com>
2024-03-14 18:52:01 +0530

    log file for requests in http server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-  console.log(req.headers);
-  res.end("Hello from server");
+  const log = `${Date.now()}: ${req.url} New Req received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    if (err) {
+      console.error("Error appending to log file:", err);
+    }
+  });
+
+  switch (req.url) {
+    case "/":
+      res.end("HomePage");
+      break;
+    case "/about":
+      res.end("Its me");
+      break;
+    default:
+      res.end("404 Not Found");
+  }
 });
 
-myServer.listen(8000, () => {
+myServer.listen(3001, () => {
   console.log("Server started");
 });
