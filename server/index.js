0ea06ecbcf36b2cc97b94bc36be807c797048213
Shahi77 <shahiinfinity02@gmail.com>
2024-03-14 19:20:26 +0530

    handling url's

--- server/index.js
+++ server/index.js
@@ -1,20 +1,26 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New Req received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     if (err) {
       console.error("Error appending to log file:", err);
     }
   });
 
-  switch (req.url) {
+  switch (myUrl.pathname) {
     case "/":
       res.end("HomePage");
       break;
     case "/about":
-      res.end("Its me");
+      const username = myUrl.query.myname;
+      res.end(`Hi, ${username}`);
+      //res.end("My name is Shahi");
       break;
     default:
       res.end("404 Not Found");
