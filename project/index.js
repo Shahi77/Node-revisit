4798e64d83aa64c2481b5fdd6c9255ad40583389
Shahi77 <shahiinfinity02@gmail.com>
2024-03-16 11:05:48 +0530

    grouping of methods

--- project/index.js
+++ project/index.js
@@ -16,22 +16,24 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-  return res.json(user);
-});
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch("/api/users/:id", (req, res) => {
+    //Edit the user with id
+    return res.json({ status: pending });
+  })
+  .delete("/api/users/:id", (req, res) => {
+    // Delete the user with id
+    return res.json({ status: pending });
+  });
 
 app.post("/api/users", (req, res) => {
   // Create new user
-  return res.json({ status: pending });
-});
-app.patch("/api/users/:id", (req, res) => {
-  //Edit the user with id
-  return res.json({ status: pending });
-});
-app.delete("/api/users/:id", (req, res) => {
-  // Delete the user with id
   return res.json({ status: pending });
 });
 
