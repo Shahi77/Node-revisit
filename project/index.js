d280cd92484d7ba72017547b8d4c5f405e1a25c6
Shahi77 <shahiinfinity02@gmail.com>
2024-03-16 11:35:34 +0530

    implementing POST

--- project/index.js
+++ project/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 3002;
+
+app.use(express.urlencoded({ extended: false })); //Middleware-plugin
 
 app.get("/users", (req, res) => {
   const html = `
@@ -16,25 +19,27 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app
-  .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-  })
-  .patch("/api/users/:id", (req, res) => {
-    //Edit the user with id
-    return res.json({ status: pending });
-  })
-  .delete("/api/users/:id", (req, res) => {
-    // Delete the user with id
-    return res.json({ status: pending });
-  });
+app.route("/api/users/:id").get((req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+//   .patch("/api/users/:id", (req, res) => {
+//     //Edit the user with id
+//     return res.json({ status: pending });
+//   })
+//   .delete("/api/users/:id", (req, res) => {
+//     // Delete the user with id
+//     return res.json({ status: pending });
+//   });
 
 app.post("/api/users", (req, res) => {
   // Create new user
-  return res.json({ status: pending });
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length });
+  });
 });
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
