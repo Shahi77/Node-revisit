657861e90f06eed0501094beeced2af152c8d26a
Shahi77 <shahiinfinity02@gmail.com>
2024-03-16 11:39:36 +0530

    implementing PATCH and DELETE

--- project/index.js
+++ project/index.js
@@ -19,19 +19,40 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.route("/api/users/:id").get((req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-  return res.json(user);
-});
-//   .patch("/api/users/:id", (req, res) => {
-//     //Edit the user with id
-//     return res.json({ status: pending });
-//   })
-//   .delete("/api/users/:id", (req, res) => {
-//     // Delete the user with id
-//     return res.json({ status: pending });
-//   });
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch("/api/users/:id", (req, res) => {
+    //Edit the user with id
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    users[userIndex] = { ...users[userIndex], ...req.body };
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({ status: "success", updatedUser: users[userIndex] });
+    });
+  })
+  .delete("/api/users/:id", (req, res) => {
+    // Delete the user with id
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    users.splice(userIndex, 1);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({
+        status: "success",
+        message: "User deleted successfully",
+      });
+    });
+  });
 
 app.post("/api/users", (req, res) => {
   // Create new user
