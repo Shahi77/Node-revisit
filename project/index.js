f3128b130afbd8f28dd69c0fe63adb7dd4a43a0e
Shahi77 <shahiinfinity02@gmail.com>
2024-03-17 11:05:12 +0530

    custom middlewares

--- project/index.js
+++ project/index.js
@@ -6,6 +6,16 @@ const PORT = 3002;
 
 app.use(express.urlencoded({ extended: false })); //Middleware-plugin
 
+app.use((req, res, next) => {
+  fs.appendFile(
+    "./log.txt",
+    `${Date.now()}: ${req.method}: ${req.path}\n`,
+    (err, data) => {
+      next();
+    }
+  );
+});
+
 app.get("/users", (req, res) => {
   const html = `
     <ul>
@@ -19,40 +29,11 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app
-  .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-  })
-  .patch("/api/users/:id", (req, res) => {
-    //Edit the user with id
-    const id = Number(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === id);
-    if (userIndex === -1) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    users[userIndex] = { ...users[userIndex], ...req.body };
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.json({ status: "success", updatedUser: users[userIndex] });
-    });
-  })
-  .delete("/api/users/:id", (req, res) => {
-    // Delete the user with id
-    const id = Number(req.params.id);
-    const userIndex = users.findIndex((user) => user.id === id);
-    if (userIndex === -1) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    users.splice(userIndex, 1);
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.json({
-        status: "success",
-        message: "User deleted successfully",
-      });
-    });
-  });
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
 
 app.post("/api/users", (req, res) => {
   // Create new user
